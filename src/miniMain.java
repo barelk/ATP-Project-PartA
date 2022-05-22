9e8de361638c435b69421afdd36df587eaefeaa3
itayrazum <itayrazum@gmail.com>
2022-05-22 20:25:36 +0300

    c

---
+++ src/miniMain.java
@@ -0,0 +1,18 @@
+import algorithms.mazeGenerators.EmptyMazeGenerator;
+import algorithms.mazeGenerators.IMazeGenerator;
+import algorithms.mazeGenerators.Maze;
+import algorithms.mazeGenerators.SimpleMazeGenerator;
+
+public class miniMain {
+
+        public static void main(String[] args) {
+
+                IMazeGenerator maze = new EmptyMazeGenerator();
+                Maze myMaze = maze.generate(10,10);
+                myMaze.myPrint();
+                //System.out.print(myMaze);
+                //System.out.println(maze.measureAlgorithmTimeMillis(500,57878));
+
+
+        }
+}
